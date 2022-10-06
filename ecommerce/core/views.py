cba3edcfecfb1872e800dc965b7e33aa7bb1e2c0
Enzujp <100221308+Enzujp@users.noreply.github.com>
2022-10-06 16:01:37 +0100

    created templates and urls.py

--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse ("This is the page that confirms our set up is up and running!")
