28d1cc8b1bee024e775b105c2e0499d876f4f970
Enzujp <100221308+Enzujp@users.noreply.github.com>
2022-10-06 15:54:54 +0100

    set postgress as database for project, created core app

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'core',
+        'USER': 'postgres',
+        'PASSWORD': 'jessedavid',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
