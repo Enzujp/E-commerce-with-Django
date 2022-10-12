385f2131e69662da8f8dc4e9b64529039cd4f610
Enzujp <100221308+Enzujp@users.noreply.github.com>
2022-10-12 18:32:52 +0100

    added category

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,10 +1,14 @@
+from unicodedata import category
 from django.shortcuts import render, get_object_or_404
 
-from .models import Product
+from .models import Product, Category
 # Create your views here.
 
 def category_detail(request, slug):
-    return render(request, 'store/category.html', )
+    category = get_object_or_404(Category, slug=slug)
+    return render(request, 'store/category.html', {
+        'category': category
+    } )
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug)
