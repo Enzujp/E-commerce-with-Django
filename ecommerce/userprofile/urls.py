bdb8edc379bf5a9e70bab6ebe84b483e9a8f22b9
Enzujp <100221308+Enzujp@users.noreply.github.com>
2022-10-18 17:28:27 +0100

    more updates

--- ecommerce/userprofile/urls.py
+++ ecommerce/userprofile/urls.py
@@ -4,6 +4,7 @@ from .import views
 
 urlpatterns = [
     path('signup/', views.signup, name="signup"),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('myaccount/', views.myaccount, name="myaccount"),
     path('vendors/<int:pk>/', views.vendor_detail, name="vendor_detail")
