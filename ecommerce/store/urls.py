2ff75f6db2d3d46447e05f882621576c6f3278f4
Enzujp <100221308+Enzujp@users.noreply.github.com>
2022-11-20 08:35:17 +0100

    been a minute

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -5,6 +5,12 @@ from . import views
 
 urlpatterns = [ 
     path('search/',  views.search, name="search"),
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name="add_to_cart"),
+    path('change-quantity/<str:product_id>/', views.change_quantity, name="change_quantity"),
+    path('remove-from-cart/<str:product_id>/', views.remove_from_cart, name="remove_from_cart"),
+    path('cart/', views.cart_view, name="cart_view"),
     path('<slug:slug>/', views.category_detail, name="category_detail"),
-    path('<slug:category_slug>/<slug:slug>/', views.product_detail, name="product_detail")
+    path('<slug:category_slug>/<slug:slug>/', views.product_detail, name="product_detail"),
+    
+    
 ]
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,7 +1,42 @@
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
+from .cart import Cart
 from .models import Product, Category
+
+
+def add_to_cart(request, product_id):
+    cart = Cart(request)
+    cart.add(product_id)
+
+    return redirect('cart_view')
+
+def change_quantity(request, product_id):
+    action = request.GET.get('action', '')
+
+    if action:
+        quantity = 1
+
+        if action == 'decrease':
+            quantity = -1
+
+        cart = Cart(request)
+        cart.add(product_id, quantity, True)
+
+    return redirect('cart_view')
+
+def remove_from_cart(request, product_id):
+    cart = Cart(request)
+    cart.remove((product_id) )
+
+    return redirect('cart_view')
+
+def cart_view(request):
+    cart = Cart(request)
+
+    return render(request, 'store/cart_view.html', {
+        'cart': cart
+    })
 
 def search(request):
     query = request.GET.get('query', '')
@@ -20,7 +55,8 @@ def category_detail(request, slug):
         'products': products
     } )
 
-def product_detail(request, category_slug, slug):
+def product_detail(request, category_slug, slug): 
+
     product = get_object_or_404(Product, slug=slug, status=Product.ACTIVE)
 
     return render(request, 'store/product_detail.html', {
