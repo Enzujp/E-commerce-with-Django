b6f3575e2fb915bd67e7fc4632ff29c14066472d
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-03-24 13:45:05 +0100

    DEBUG = False

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i5%fs5-6@*!)tct@61)xzy)@zw*uh77mcrp!$6tby2#h8u$mu^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
