a14d5ed9ffc87d75fa559d97753f254c2dc39d8b
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-06-19 10:55:11 +0100

    updated todo list

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -138,6 +140,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
