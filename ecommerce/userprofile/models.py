705c4fb0d2006d0bb111db5a7b4248b000a97c97
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-06-20 13:02:20 +0100

    added comments to userprofile model

--- ecommerce/userprofile/models.py
+++ ecommerce/userprofile/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Userprofile(models.Model):
+    """Userprofile class to seperate vendors from customers"""
     user = models.OneToOneField(User, related_name="userprofile", on_delete=models.CASCADE)
     is_vendor = models.BooleanField(default=False)
 
